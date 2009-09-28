706bd271e77c9e37fbf5eb3dd76a405d874ccc22
Daniel Tralamazza <tralamazza@gmail.com>
2009-09-28 15:47:36 +0200

    Moved files around, trying to make this a gem

---
+++ lib/thread_tools/semaphore.rb
@@ -0,0 +1,37 @@
+# Author: Daniel Tralamazza
+# Date: 
+#
+# Semaphore
+#
+
+
+require 'thread'
+
+
+module ThreadTools
+
+    # Poor man's  semaphore
+    class Semaphore
+        attr_reader :count
+        def initialize(_count)
+            @count = _count
+            @mtx = Mutex.new
+            @cv = ConditionVariable.new
+        end
+
+        def acquire
+            @mtx.synchronize do
+                @cv.wait(@mtx) until @count > 0
+                @count -= 1
+            end
+        end
+
+        def release
+            @mtx.synchronize do
+                @count += 1
+                @cv.signal
+            end
+        end
+    end
+
+end
