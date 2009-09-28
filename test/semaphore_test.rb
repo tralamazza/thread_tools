4bf6f990b8aa71887bf5ada17dcda813f15942ea
Daniel Tralamazza <tralamazza@gmail.com>
2009-09-28 17:00:23 +0200

    Semaphore test

---
+++ test/semaphore_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/unit'
+require File.expand_path(File.dirname(__FILE__)+'/../lib/thread_tools/semaphore')
+
+
+MiniTest::Unit.autorun
+
+
+class SemaphoreTest < MiniTest::Unit::TestCase
+    def setup
+        super
+        @sem = ThreadTools::Semaphore.new(1)
+    end
+
+    def test_acquire
+        assert_equal(@sem.acquire, 0)
+    end
+
+    def test_release
+        ok = false
+        Thread.new do
+            @sem.acquire
+            ok = true
+        end
+        sleep 0.5
+        @sem.release
+        # release should unblock acquire
+        assert_equal(ok, true)
+        # semaphore count has to be 1
+        assert_equal(@sem.count, 1)
+    end
+end
