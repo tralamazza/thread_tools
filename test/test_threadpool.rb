6c686dc199fd88d6718fff945acc48cb598cc11c
Daniel Tralamazza <tralamazza@gmail.com>
2009-10-14 19:11:54 +0200

    Break down test case
    Multiple smaller test cases
    New test case for create_on_spawn flag

--- test/test_threadpool.rb
+++ test/test_threadpool.rb
@@ -6,36 +6,53 @@ class ThreadPoolTest < Test::Unit::TestCase
     def setup
         super
         @tpool = ThreadTools::ThreadPool.new(2)
-        @tpool.kill_worker_on_exception = true
     end
 
-    def test_thread_spawn
+    def test_spawn
         uniq_threads = {}
         sum_orig = 0
-        sum_lock = Mutex.new
         sum_thr = 0
-        4.times {|i|
+        sum_lock = Mutex.new
+        4.times do |i|
             sum_orig += i
-            @tpool.spawn(i) {|ti|
-                sum_lock.synchronize do
-                    sum_thr += ti
-                end
+            @tpool.spawn(i) do |j|
+                sum_lock.synchronize { sum_thr += j }
                 uniq_threads[Thread.current] = true
-                if (ti == 3)
-                  # this exception should kill this worker
-                  raise "oups"
-                end
-            }
-        }
+            end
+        end
         sleep 0.1
-        # 2 workers - 1 from raised exceptions = 1
-        assert_equal(@tpool.size, 1)
-        @tpool.shutdown
         # same sum
         assert_equal(sum_orig, sum_thr)
         # number of different threads
         assert_equal(uniq_threads.size, 2)
+    end
+ 
+    def test_raise
+        @tpool.kill_worker_on_exception = true
+        2.times do |i|
+            @tpool.spawn(i+1) do |j|
+                raise 'oups' if j == 2
+            end
+        end
+        sleep 0.1
+        # 2 workers - 1 from raised exceptions = 1
+        assert_equal(@tpool.size, 1)
+    end
+ 
+    def test_shutdown
+        # initial number of threads
+        assert_equal(@tpool.size, 2)
+        @tpool.shutdown
         # after shutdown all threads are dead
         assert_equal(@tpool.size, 0)
+    end
+
+    def test_spawn_create
+        # kill all workers first
+        @tpool.shutdown
+        @tpool.create_on_spawn = true
+        @tpool.spawn {}
+        # a new worker was created
+        assert_equal(@tpool.size, 1)
     end
 end
