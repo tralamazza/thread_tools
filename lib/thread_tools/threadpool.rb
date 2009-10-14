632780805131eda71c523791faca72f2e81ab291
Daniel Tralamazza <tralamazza@gmail.com>
2009-10-14 19:19:01 +0200

    Added a "busy" thread group, better .shutdown
    - Shutdown now raises an exception in every active worker
    - Threads are added in a dummy group when the block is finished, this removes
    it from the busy group

--- lib/thread_tools/threadpool.rb
+++ lib/thread_tools/threadpool.rb
@@ -33,6 +33,7 @@ module ThreadTools
         # if set to true a new worker is created if the pool is empty
         attr_accessor :create_on_spawn
 
+
         # *_size* should be at least 1, *_thr_group* (optional) thread group
         def initialize(_size, _thr_group = nil)
             @kill_worker_on_exception = false
@@ -40,7 +41,8 @@ module ThreadTools
             @pool_mtx = Mutex.new
             @pool_cv = ConditionVariable.new
             @pool = []
-            @thr_grp = _thr_group
+            @dummy_grp = ThreadGroup.new
+            @busy_grp = _thr_group.nil? ? ThreadGroup.new : _thr_group
             @create_on_spawn = false
             _size = 1 if _size < 1
             _size.times { create_worker }
@@ -48,35 +50,31 @@ module ThreadTools
 
         def create_worker
             Thread.new do
-                thr = Thread.current
-                @pool_mtx.synchronize do
-                    @size += 1
-                    if (!@thr_grp.nil?)
-                        @thr_grp.add(thr)
-                    end
-                end
-                thr[:jobs] = []         # XXX array not really necessary
-                thr[:sem] = Semaphore.new(0)
-                loop do
-                    @pool_mtx.synchronize do
-                        @pool << thr    # puts this thread in the pool
-                        @pool_cv.signal
-                    end
-                    thr[:sem].acquire   # wait here for jobs to become available
-                    job = thr[:jobs].shift  # pop out a job
-                    if (job.nil? || job[:block].nil?)
-                        break           # exit thread if job or block is nil
-                    end
-                    begin
-                        job[:block].call(*job[:args])       # call block
-                    rescue
-                        if (@kill_worker_on_exception)
-                            break       # exit thread on exception
+                @pool_mtx.synchronize do @size += 1 end
+                begin
+                    thr = Thread.current
+                    thr[:jobs] = []         # XXX array not really necessary
+                    thr[:sem] = Semaphore.new(0)
+                    loop do
+                        @pool_mtx.synchronize do
+                            @pool << thr    # puts this thread in the pool
+                            @pool_cv.signal
+                        end
+                        thr[:sem].acquire   # wait here for jobs to become available
+                        job = thr[:jobs].shift  # pop out a job
+                        if (job.nil? || job[:block].nil?)
+                            break           # exit thread if job or block is nil
+                        end
+                        begin
+                            @busy_grp.add(thr)  # adds the thread
+                            job[:block].call(*job[:args])   # call block
+                            @dummy_grp.add(thr) # removes from the previous group
+                        rescue
+                            break if @kill_worker_on_exception  # exit thread on exception
                         end
                     end
-                end
-                @pool_mtx.synchronize do
-                    @size -= 1
+                ensure
+                    @pool_mtx.synchronize do @size -= 1 end
                 end
             end
         end
@@ -85,9 +83,7 @@ module ThreadTools
             thr = nil
             @pool_mtx.synchronize do
                 # creates a new worker thread if pool is empty and flag is set
-                if (@create_on_spawn && @pool.empty?)
-                    create_worker
-                end
+                create_worker if @create_on_spawn && @pool.empty?
                 # wait here until a worker is available
                 @pool_cv.wait(@pool_mtx) until !(thr = @pool.shift).nil?
                 thr[:jobs] << { :args => args, :block => block }
@@ -97,18 +93,26 @@ module ThreadTools
         end
 
         def shutdown(_sync = true)
-            thr = nil
             while !@pool.empty? do
+                thr = nil
                 @pool_mtx.synchronize do
                     @pool_cv.wait(@pool_mtx) until !(thr = @pool.shift).nil?
                 end
                 thr[:jobs].clear    # clear any pending job
                 thr[:jobs] << nil   # queue a nil job
                 thr[:sem].release
-                if (_sync)
-                    thr.join        # wait here for the thread to die
-                end
+                thr.join if _sync   # wait here for the thread to die
             end
+            busy_list = @busy_grp.list
+            busy_list.each do |thr|
+                thr.raise('shutdown')   # XXX we could .kill instead
+                thr.join if _sync       # wait here for the thread to die
+            end
+        end
+
+        # returns the number of busy workers
+        def busy_size
+            @busy_grp.list.size
         end
     end
 
